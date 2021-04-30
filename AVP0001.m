51d05334a16bacbf5d814f421b77e54eea6c7ea7
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 23:34:49 +0700

    movement of some AVs
    of stack 0, to gate 2

--- AVP0001.m
+++ AVP0001.m
@@ -24,6 +24,18 @@ res = moveAV_Row0(0, 8, v0);
 %===movement of AV[1,2]===
 res = moveAV_Row0(1, 2, v0);
 
+%===movement of AV[2,4]===
+res = moveAV2Gate2(2, 4, v0);
+
+%===movement of AV[2,5]===
+res = moveAV2Gate2(2, 5, v0);
+
+%===movement of AV[2,7]===
+res = moveAV2Gate2(2, 7, v0);
+
+%===movement of AV[1,7]===
+res = moveAV2Gate2(1, 7, v0);
+
 axis([-15 130 -15 70]);%expand minimum of X, Y
 view(0, -90);
 title ("Automated Valet Parking v = 24km/h");
