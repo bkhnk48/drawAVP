48d2f68e59ca0cf758841f63978f75cd9a6af262
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 17:58:50 +0700

    curve movement function

---
+++ curveMovement.m
@@ -0,0 +1,20 @@
+function [lastestX, lastestY, lastT] = curveMovement(a0, alpha, velocity, startTime, x0, y0, xCenter, yCenter, R, color)
+  w0 = velocity / R;
+  deltaT = abs(alpha)/w0;
+  if(alpha < 0)
+    w0 = -w0;
+  endif;
+  t = linspace(startTime, startTime + deltaT, round(deltaT*24));  
+  a = a0 + w0*(t - startTime);
+  x = xCenter + R*cos(a);
+  y = yCenter + R*sin(a);
+  hold on;
+  if(color == 'r')
+    plot(x, y, 'r');
+  elseif(color == 'k')
+    plot(x, y, 'k');
+  endif;
+  lastestX = x(length(x));
+  lastestY = y(length(y));
+  lastT = startTime + deltaT;
+end
