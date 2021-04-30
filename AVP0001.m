2553ad05be1195c2a775768e18e23f8f7cfb3b15
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 07:54:37 +0700

    Draw 4 gates and modify minimum values of x/y-axis

--- AVP0001.m
+++ AVP0001.m
@@ -18,7 +18,7 @@ for i = 0: 34
       else
           rectangle("Position", [10 + i*2.7, 10 + j*5.4, 2.7, 5.4],
               "Facecolor", [1, 1, 0]);
-      endif
+      endif;
     endfor;
 endfor;
 
@@ -28,5 +28,18 @@ for i = 0: 34
               "Facecolor", [1, 1, 0]);
     endfor;
 endfor;
-axis([0 inf 0 inf]);
+
+rectangle("Position", [0, 0, 10 + 2.7*35 + 10, 10 + 5.4*3 + 10 + 5.4*3 + 10 ]);
+                      %boundary
+
+rectangle("Position", [10 + 2.7*17.5 - 10.4, -5, 20.8, 5 ], 
+                        "Facecolor", [0, 0, 0]);%Gate 1
+rectangle("Position", [10 + 2.7*35 + 10, 20.8, 5, 20.8 ], 
+                        "Facecolor", [0, 0, 1]);%Gate 2
+rectangle("Position", [10 + 2.7*17.5 - 10.4, 10 + 5.4*3 + 10 + 5.4*3 + 10, 20.8, 5 ], 
+                        "Facecolor", [1, 0, 0]);%Gate 3                        
+rectangle("Position", [-5, 20.8, 5, 20.8 ], "Facecolor", 
+                          [0.65, 0.16, 0.16]);%Gate 4
+                        
+axis([-5 inf -5 inf]);%expand minimum of X, Y
 view(0, -90);
