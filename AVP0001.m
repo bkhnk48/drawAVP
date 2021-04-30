38ca5f0f5336c01d96c0d38eb2a68bd417de39f6
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 22:59:45 +0700

    movement of a new AV to gate 3

--- AVP0001.m
+++ AVP0001.m
@@ -7,19 +7,22 @@ res = init();
 v0 = 20/3;%(m/s)
 
 %===movement of AV[0,0]===
-res = moveAV_Row0(0, v0);
+res = moveAV_Row0(0, 0, v0);
                     
 %===movement of AV[0,2]===
-res = moveAV_Row0(2, v0);
+res = moveAV_Row0(0, 2, v0);
 
 %===movement of AV[0,4]===
-res = moveAV_Row0(4, v0);
+res = moveAV_Row0(0, 4, v0);
 
 %===movement of AV[0,7]===
-res = moveAV_Row0(7, v0);
+res = moveAV_Row0(0, 7, v0);
 
 %===movement of AV[0,8]===
-res = moveAV_Row0(8, v0);
+res = moveAV_Row0(0, 8, v0);
+
+%===movement of AV[1,2]===
+res = moveAV_Row0(1, 2, v0);
 
 axis([-15 130 -15 70]);%expand minimum of X, Y
 view(0, -90);
