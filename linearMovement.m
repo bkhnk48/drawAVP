055ba59a9921ead2dbdca85bef0f055dacdab9ae
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 16:17:02 +0700

    function for linear movement

---
+++ linearMovement.m
@@ -0,0 +1,13 @@
+function [lastestX, lastestY] = linearMovement(alpha, velocity, startTime, endTime, x0, y0, color)
+  t = linspace(startTime, endTime, round((endTime - startTime)*24));
+  x = x0 + velocity*(t - startTime)*cos(alpha);
+  y = y0 + velocity*(t - startTime)*sin(alpha);
+  hold on;
+  if(color == 'r')
+    plot(x, y, 'r');
+  elseif(color == 'k')
+    plot(x, y, 'k');
+  endif
+  lastestX = x(length(x));
+  lastestY = y(length(y));
+ end
