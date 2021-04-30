5b943988e69cbf97c95900cc2282f80a8093792e
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-04-30 22:42:33 +0700

    trajectories of some AV in the same row

--- AVP0001.m
+++ AVP0001.m
@@ -7,9 +7,20 @@ res = init();
 v0 = 20/3;%(m/s)
 
 %===movement of AV[0,0]===
-res = moveAV_0_0(v0);
+res = moveAV_Row0(0, v0);
                     
-                  
+%===movement of AV[0,2]===
+res = moveAV_Row0(2, v0);
+
+%===movement of AV[0,4]===
+res = moveAV_Row0(4, v0);
+
+%===movement of AV[0,7]===
+res = moveAV_Row0(7, v0);
+
+%===movement of AV[0,8]===
+res = moveAV_Row0(8, v0);
+
 axis([-15 130 -15 70]);%expand minimum of X, Y
 view(0, -90);
 title ("Automated Valet Parking v = 24km/h");
