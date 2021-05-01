c24c60fe22d0f5f1b53ce30754e838147070f8d2
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 02:50:06 +0700

    remove color param

--- linearMovement.m
+++ linearMovement.m
@@ -1,4 +1,4 @@
-function [Coord, lastT] = linearMovement(alpha, velocity, startTime, endTime, x0, y0, color)
+function [Coord, lastT] = linearMovement(alpha, velocity, startTime, endTime, x0, y0)
   t = linspace(startTime, endTime, round((endTime - startTime)*24));
   x = x0 + velocity*(t - startTime)*cos(alpha);
   y = y0 + velocity*(t - startTime)*sin(alpha);
