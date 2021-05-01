932a7100a67e8fbc76744ea01ff82574bdffa489
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 02:49:38 +0700

    curve movement and remove redundant param of
    linear movement

--- moveAV_Row0.m
+++ moveAV_Row0.m
@@ -4,7 +4,20 @@ function [res] = moveAV_Row0(stack, row, column, v0)
   
   deltaY = (5 - (row + (1 - stack)*3))*5.4;
   [FirstTrajectory, lastT] = linearMovement(pi/2, v0, 0, (row + 1)*5.4/v0, 10 + 1.35 + deltaX, 
-                                        10 + stack*10 + 2.7 + deltaY, 'r');
+                                        10 + stack*10 + 2.7 + deltaY);
   
-  res = FirstTrajectory;
+  Radius = sqrt(1.35*1.35 + 6.7*6.7) ;%radius of circle trajectory (m)
+  a0 = atan(1.35/6.7);%original angular (rad)
+
+  XY = FirstTrajectory(:, columns(FirstTrajectory));
+  x0 = XY(1, 1);
+  y0 = XY(2, 1);
+  xCenter = x0 - 6.7;
+  yCenter = y0 - 1.35;
+
+  [SecondTrajectory, lastT] = curveMovement(a0, pi/2, v0, lastT, x0, y0, 
+                      xCenter, yCenter, Radius, 'r');
+                      
+  %res = [FirstTrajectory, SecondTrajectory];
+  res = SecondTrajectory;
 endfunction
