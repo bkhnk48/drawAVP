4041b09bea0f1fd35532f9fab30bd9d9619d444b
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 01:14:14 +0700

    the function returns a matrix (coord of object)

--- linearMovement.m
+++ linearMovement.m
@@ -1,10 +1,8 @@
-function [lastestX, lastestY, lastT] = linearMovement(alpha, velocity, startTime, endTime, x0, y0, color)
+function [Coord, lastT] = linearMovement(alpha, velocity, startTime, endTime, x0, y0, color)
   t = linspace(startTime, endTime, round((endTime - startTime)*24));
   x = x0 + velocity*(t - startTime)*cos(alpha);
   y = y0 + velocity*(t - startTime)*sin(alpha);
-  hold on;
-  plot(x, y, color);
-  lastestX = x(length(x));
-  lastestY = y(length(y));
+  
+  Coord = [x; y];
   lastT = endTime;
  endfunction
