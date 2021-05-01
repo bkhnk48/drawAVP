ebe06e700a5021081bc58e3c98bcc0ddccb86ea3
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 01:03:54 +0700

    AVP has videos

--- AVP0001.m
+++ AVP0001.m
@@ -2,40 +2,40 @@ clf;
 axis equal;
 
 
-res = init();
+%res = init();
 
 v0 = 20/3;%(m/s)
-
+%figure(1);
 %===movement of AV[0,0]===
-res = moveAV_Row0(0, 0, v0);
+res = moveAV_Row0(1, 0, 0, v0);
                     
 %===movement of AV[0,2]===
-res = moveAV_Row0(0, 2, v0);
+%res = moveAV_Row0(0, 2, v0);
 
 %===movement of AV[0,4]===
-res = moveAV_Row0(0, 4, v0);
+%res = moveAV_Row0(0, 4, v0);
 
 %===movement of AV[0,7]===
-res = moveAV_Row0(0, 7, v0);
+%res = moveAV_Row0(0, 7, v0);
 
 %===movement of AV[0,8]===
-res = moveAV_Row0(0, 8, v0);
+%res = moveAV_Row0(0, 8, v0);
 
 %===movement of AV[1,2]===
-res = moveAV_Row0(1, 2, v0);
+%res = moveAV_Row0(1, 2, v0);
 
 %===movement of AV[2,4]===
-res = moveAV2Gate2(2, 4, v0);
+%res = moveAV2Gate2(2, 4, v0);
 
 %===movement of AV[2,5]===
-res = moveAV2Gate2(2, 5, v0);
+%res = moveAV2Gate2(2, 5, v0);
 
 %===movement of AV[2,7]===
-res = moveAV2Gate2(2, 7, v0);
+%res = moveAV2Gate2(2, 7, v0);
 
 %===movement of AV[1,7]===
-res = moveAV2Gate2(1, 7, v0);
+%res = moveAV2Gate2(1, 7, v0);
 
-axis([-15 130 -15 70]);%expand minimum of X, Y
-view(0, -90);
-title ("Automated Valet Parking v = 24km/h");
+%axis([-15 130 -15 70]);%expand minimum of X, Y
+%view(0, -90);
+%title ("Automated Valet Parking v = 24km/h");
