a68a0eaa3cf3e4a0bd872483875d77a27b1c208e
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 02:51:19 +0700

    change return value of func

--- curveMovement.m
+++ curveMovement.m
@@ -1,4 +1,4 @@
-function [lastestX, lastestY, lastT] = curveMovement(a0, alpha, velocity, startTime, x0, y0, xCenter, yCenter, R, color)
+function [Coord, lastT] = curveMovement(a0, alpha, velocity, startTime, x0, y0, xCenter, yCenter, R, color)
   w0 = velocity / R;
   deltaT = abs(alpha)/w0;
   if(alpha < 0)
@@ -8,9 +8,7 @@ function [lastestX, lastestY, lastT] = curveMovement(a0, alpha, velocity, startT
   a = a0 + w0*(t - startTime);
   x = xCenter + R*cos(a);
   y = yCenter + R*sin(a);
-  hold on;
-  plot(x, y, color);
-  lastestX = x(length(x));
-  lastestY = y(length(y));
+  Coord = [x; y];
   lastT = startTime + deltaT;
+  
 endfunction
