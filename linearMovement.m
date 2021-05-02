41fdfaabb976f5137fa7283eb32a5c476bba849f
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-02 17:27:57 +0700

    Add angle of velocity

--- linearMovement.m
+++ linearMovement.m
@@ -2,7 +2,10 @@ function [Coord, lastT] = linearMovement(alpha, velocity, startTime, endTime, x0
   t = linspace(startTime, endTime, round((endTime - startTime)*24));
   x = x0 + velocity*(t - startTime)*cos(alpha);
   y = y0 + velocity*(t - startTime)*sin(alpha);
-  
-  Coord = [x; y];
+  alpha = alpha - (pi/2);
+  v = [alpha];
+  v = repelem(v, length(y));
+  v = v';
+  Coord = [x; y; v];
   lastT = endTime;
  endfunction
