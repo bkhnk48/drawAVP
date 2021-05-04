2473e28e5041174ba1583c8c86e7a457d2c25179
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-04 18:42:45 +0700

    movement of the 4th AV

--- AVP0001.m
+++ AVP0001.m
@@ -7,10 +7,10 @@ axis equal;
 v0 = 20/3;%(m/s)
 %figure(1);
 %===movement of AV[0,0]===
-[res1] = moveAV_Row0(1, 0, 0, v0);
+[res1] = moveAV_Row0(1, 0, 0, v0, []);
                     
 %===movement of AV[0,2]===
-%res = moveAV_Row0(0, 2, v0);
+[res2] = moveAV_Row0(1, 0, 2, v0, res1);
 
 %===movement of AV[0,4]===
 %res = moveAV_Row0(0, 4, v0);
@@ -28,15 +28,9 @@ v0 = 20/3;%(m/s)
 %[res2] = moveAV2Gate2(1, 2, 4, v0);
 
 %===movement of AV[2,5]===
-%res = moveAV2Gate2(2, 5, v0);
-
-%===movement of AV[2,7]===
-%res = moveAV2Gate2(2, 7, v0);
+[res3] = moveAV2Gate2(1, 2, 5, v0, res2);
 
 %===movement of AV[1,7]===
-[res2] = moveAV2Gate2(1, 2, 7, v0, res1);
+[res4] = moveAV2Gate2(1, 2, 7, v0, res3);
 
-%axis([-15 130 -15 70]);%expand minimum of X, Y
-%view(0, -90);
-%title ("Automated Valet Parking v = 24km/h");
-ret = animate(res2);
+ret = animate(res4);
