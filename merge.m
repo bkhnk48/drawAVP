8953ac2e3fbc77dc255ac1aa4dd4c7589b3f93c1
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-04 22:18:13 +0700

    merge matrices with different numbers of rows fno

--- merge.m
+++ merge.m
@@ -2,6 +2,7 @@ function [res] = merge(matrix1, matrix2)
   col1 = columns(matrix1);
   col2 = columns(matrix2);
   res = matrix1;
+  %nRows = max(rows(matrix1), rows(matrix2));
   
   n = abs(col1 - col2);
   if( n != 0)
@@ -12,10 +13,26 @@ function [res] = merge(matrix1, matrix2)
     v = repelem(v, n);
     v = v';
     u = [u; u; u; v];
+    
     if(col2 < col1)
-      matrix2 = [matrix2, u];
+      appendMatrix = u;
+      nRows = rows(matrix2) - 4;
+      if(nRows > 1)
+        for i = 1: (nRows/4)
+          appendMatrix = [u; appendMatrix];
+        endfor;
+      endif;
+      matrix2 = [matrix2, appendMatrix];
     else
-      matrix1 = [matrix1, u];
+      
+      appendMatrix = u;
+      nRows = rows(matrix1) - 4;
+      if(nRows > 1)
+        for i = 1: (nRows/4)
+          appendMatrix = [u; appendMatrix];
+        endfor;
+      endif;
+      matrix1 = [matrix1, appendMatrix];
     endif
   endif;
   res = [matrix1; matrix2];
