1aa98bfb1490dbfa2084ed641769e437a7d8fd09
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2021-05-04 14:48:33 +0700

    call merge func to combine two matrices

--- moveAV2Gate2.m
+++ moveAV2Gate2.m
@@ -24,23 +24,6 @@ function [res] = moveAV2Gate2(stack, row, column, v0, otherCar)
   [ThirdTrajectory, lastT] = linearMovement(0, v0, lastT, lastT + (WIDTH - x0)/v0, x0, 
                                         y0);  
   res = [FirstTrajectory, SecondTrajectory, ThirdTrajectory];
-  col2 = columns(res);
-  col1 = columns(otherCar);
   
-  n = abs(col1 - col2);
-  if( n != 0)
-    u = [0]; 
-    u = repelem(u, n);
-    u = u';
-    v = [-1];
-    v = repelem(v, n);
-    v = v';
-    u = [u; u; u; v];
-    if(col2 < col1)
-      res = [res, u];
-    elseif
-      otherCar = [otherCar, u];
-    endif
-  endif;
-  res = [otherCar; res];
+  [res] = merge(otherCar, res);
 endfunction
