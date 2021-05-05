d6b6dfba0fdcf838c18ff74598482d55bc6576ea
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-05-05 16:26:36 +0700

    call configuration func instead of calculating values

--- moveAV2Gate1.m
+++ moveAV2Gate1.m
@@ -1,12 +1,10 @@
 function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
   lastT = 0;
   res = [];
-  WIDTH = 10 + 35*2.7 + 10;%width of car parking lot
-  HEIGHT = 30 + 5.4*6;
   deltaX = column*2*1.35;
-  
   deltaY = (5 - (row + (1 - stack)*3))*5.4;%stack tinh tu duoi len (!)
   
+  [WIDTH, HEIGHT, Radius, a0] = configuration();
   %if(column == 1)
   %  [ZeroTrajectory, lastT] = waitFor(0, 4.840754, 10 + 1.35 + deltaX, 
   %                                      10 + 2.7 + deltaY + 10, 0);
@@ -14,10 +12,7 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
   %endif;
   [FirstTrajectory, lastT] = linearMovement(-pi/2, v0, 0 + lastT, lastT + (3 - row)*5.4/v0, 10 + 1.35 + deltaX, 
                                         10 + 2.7 + deltaY + 10);
-                                        
-  Radius = sqrt(1.35*1.35 + 6.7*6.7) ;%radius of circle trajectory (m)
-  a0 = atan(1.35/6.7);%original angular (rad)
-
+  
   XY = FirstTrajectory(:, columns(FirstTrajectory));
   x0 = XY(1, 1);
   y0 = XY(2, 1);
