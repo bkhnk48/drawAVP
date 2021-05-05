579cf5c1ce89d9790acd50aadee35f3524739965
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-05-05 17:27:30 +0700

    use new return values of linearMovement func

--- moveAV2Gate1.m
+++ moveAV2Gate1.m
@@ -10,28 +10,19 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
   %                                      10 + 2.7 + deltaY + 10, 0);
   %  [res] = [ZeroTrajectory];                                    
   %endif;
-  [FirstTrajectory, lastT] = linearMovement(-pi/2, v0, 0 + lastT, lastT + (3 - row)*5.4/v0, 10 + 1.35 + deltaX, 
+  [FirstTrajectory, lastT, x0, y0] = linearMovement(-pi/2, v0, 0 + lastT, lastT + (3 - row)*5.4/v0, 10 + 1.35 + deltaX, 
                                         10 + 2.7 + deltaY + 10);
   
-  XY = FirstTrajectory(:, columns(FirstTrajectory));
-  x0 = XY(1, 1);
-  y0 = XY(2, 1);
   xCenter = x0 + 6.7;
   yCenter = y0 + 1.35;
 
   [SecondTrajectory, lastT, x0, y0] = curveMovement(a0 + pi, a0, pi/2, v0, lastT,  
                       xCenter, yCenter, Radius);
-  %XY = SecondTrajectory(:, columns(SecondTrajectory));
-  %x0 = XY(1, 1);
-  %y0 = XY(2, 1); 
     
-  [ThirdTrajectory, lastT] = linearMovement(0, v0, lastT, lastT + (WIDTH + 1.8 - 10 - x0)/v0, x0, 
+  [ThirdTrajectory, lastT, x0, y0] = linearMovement(0, v0, lastT, lastT + (WIDTH + 1.8 - 10 - x0)/v0, x0, 
                                         y0);  
   res = [res, FirstTrajectory, SecondTrajectory, ThirdTrajectory];
   
-  XY = ThirdTrajectory(:, columns(ThirdTrajectory));
-  x0 = XY(1, 1);
-  y0 = XY(2, 1);
   xCenter = x0 - 1.35;
   yCenter = y0 + 6.7;
   
@@ -39,18 +30,10 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
                       xCenter, yCenter, Radius);
   res = [res, ForthTrajectory];
   
-  %XY = ForthTrajectory(:, columns(ForthTrajectory));
-  %x0 = XY(1, 1);
-  %y0 = XY(2, 1);
-  
-  [FifthTrajectory, lastT] = linearMovement(pi/2, v0, lastT, lastT + (HEIGHT + 1.35 - 10 - y0)/v0, x0, 
+  [FifthTrajectory, lastT, x0, y0] = linearMovement(pi/2, v0, lastT, lastT + (HEIGHT + 1.35 - 10 - y0)/v0, x0, 
                                         y0); 
   res = [res, FifthTrajectory];
   
-  
-  XY = FifthTrajectory(:, columns(FifthTrajectory));
-  x0 = XY(1, 1);
-  y0 = XY(2, 1);
   xCenter = x0 - 6.7;
   yCenter = y0 - 1.35;
   
@@ -58,17 +41,10 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
                       xCenter, yCenter, Radius);
   res = [res, SixthTrajectory];
   
-  %XY = SixthTrajectory(:, columns(SixthTrajectory));
-  %x0 = XY(1, 1);
-  %y0 = XY(2, 1);
-  
-  [SeventhTrajectory, lastT] = linearMovement(pi, v0, lastT, lastT + (x0 - (WIDTH/2) - (2.7*4))/v0, x0, 
+  [SeventhTrajectory, lastT, x0, y0] = linearMovement(pi, v0, lastT, lastT + (x0 - (WIDTH/2) - (2.7*4))/v0, x0, 
                                         y0); 
   res = [res, SeventhTrajectory];
   
-  XY = SeventhTrajectory(:, columns(SeventhTrajectory));
-  x0 = XY(1, 1);
-  y0 = XY(2, 1);
   xCenter = x0 + 1.35;
   yCenter = y0 + 6.7;
   
@@ -76,11 +52,7 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
                       xCenter, yCenter, Radius);
   res = [res, EighthTrajectory];
   
-  %XY = EighthTrajectory(:, columns(EighthTrajectory));
-  %x0 = XY(1, 1);
-  %y0 = XY(2, 1);
-  
-  [NinethTrajectory, lastT] = linearMovement(pi/2, v0, lastT, lastT + (2.7/v0), x0, 
+  [NinethTrajectory, lastT, x0, y0] = linearMovement(pi/2, v0, lastT, lastT + (2.7/v0), x0, 
                                         y0); 
   res = [res, NinethTrajectory];
   
