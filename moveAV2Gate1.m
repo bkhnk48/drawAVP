1b45cdb1022e2951e5476a7a7788c193937f90c8
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2021-05-06 09:13:45 +0700

    change direction/center of curve trajectory

--- moveAV2Gate1.m
+++ moveAV2Gate1.m
@@ -5,20 +5,18 @@ function [res] = moveAV2Gate1(stack, row, column, v0, otherCar)
   deltaY = (5 - (row + (1 - stack)*3))*5.4;%stack tinh tu duoi len (!)
   
   [WIDTH, HEIGHT, Radius, a0] = configuration();
-  %if(column == 1)
-  %  [ZeroTrajectory, lastT] = waitFor(0, 4.840754, 10 + 1.35 + deltaX, 
-  %                                      10 + 2.7 + deltaY + 10, 0);
-  %  [res] = [ZeroTrajectory];                                    
-  %endif;
+  
   [FirstTrajectory, lastT, x0, y0] = linearMovement(-pi/2, v0, 0 + lastT, lastT + (3 - row)*5.4/v0, 10 + 1.35 + deltaX, 
                                         10 + 2.7 + deltaY + 10);
   
-  xCenter = x0 + 6.7;
+  xCenter = x0 - 6.7;
   yCenter = y0 + 1.35;
 
-  [SecondTrajectory, lastT, x0, y0] = curveMovement(a0 + pi, a0, pi/2, v0, lastT,  
+  [SecondTrajectory, lastT, x0, y0] = curveMovement(-a0, -a0, -pi/2, v0, lastT,  
                       xCenter, yCenter, Radius);
-    
+  
+  %printf("Time for go directly to Gate 2: %f\n", lastT);
+  
   [ThirdTrajectory, lastT, x0, y0] = linearMovement(0, v0, lastT, lastT + (WIDTH + 1.8 - 10 - x0)/v0, x0, 
                                         y0);  
   res = [res, FirstTrajectory, SecondTrajectory, ThirdTrajectory];
